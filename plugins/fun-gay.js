e9720adc41c572b87c431f7d85ea197d1ae21782
dougyk1000 <134847205+dougyk1000@users.noreply.github.com>
2023-12-21 00:37:39 +0200

    fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
   let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
   conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/gay', {
     avatar: pp, 
-  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nWho wants to rape this gay☠️? `, m)
+  }), 'gay.png', `🏳️‍🌈  *Gay :* ${name}\n\nWho wants to destroy this gay☠️? `, m)
 }
 
 handler.help = ['gay @user']
